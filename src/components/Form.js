a3b6c4583434281e3b744c1d24c8e715fb41ae80
Rifaz Nahiyan <rifazn@protonmail.com>
2022-12-09 18:54:55 +0600

    Updating values in sections.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,41 +1,39 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function Form(props) {
-  const [additionalFields, setAdditionalFields] = useState([]);
-  
   const contactFields = props.data.contactInfo.map((field, index) => {
     const [k, v] = Object.entries(field)[0];
     return createInputField(k, v, index);
   });
 
-  const sectionFields = props.data.sections.map((section) => {
-    const subsections = section.subsections.map((subsection) => {
+  const sectionFields = props.data.sections.map((section, index) => {
+    const subsections = section.subsections.map((subsection, idx) => {
       return (
         <fieldset name="subsections[]" classNames="my-1rem">
           <label>Discipline (Degree, Position, Role, etc.)
-            <input type="text" name="discipline" size="20" value={subsection.title} />
+            <input type="text" name="subject" size="20" value={subsection.subject} data-index={idx} />
           </label>
           <label>Institute (School, Employer, Project Name, etc.)
-            <input type="text" name="institute" size="20" value={subsection.subtitle} />
+            <input type="text" name="organization" size="20" value={subsection.organization} data-index={idx} />
           </label>
           <label>Description
-            <textarea rows="8" cols="50"
-              value={ subsection.description.reduce((string, line) => string + '\n\n' + line)}>
+            <textarea rows="8" cols="50" data-index={idx} name="description"
+              value={ subsection.description.reduce((string, line) => string + '\n' + line)}>
             </textarea>
           </label>
           <label>Timeperiod (Ex: Jun 2015 - Jan 2022)
-            <input type="text" name="timeperiod" size="20" value={subsection.timeperiod} />
+            <input type="text" name="timeperiod" size="20" value={subsection.timeperiod} data-index={idx} />
           </label>
         </fieldset>
       );
     });
 
     return (
-      <fieldset name="sections[]" classNames="my-2rem">
+      <fieldset name="sections[]" classNames="my-2rem" key={index} onChange={(ev) => sectionHandler(ev, index)}>
         <legend>{section.title}</legend>
 
         <label>Section Name (Ex: Education, Employment, Projects, etc.)
-          <input type="text" name="title" />
+          <input type="text" name="title" value={section.title} />
         </label>
 
         {subsections}
@@ -59,8 +57,10 @@ export default function Form(props) {
 
   function contactInfoButtonHandler(ev) {
     const btn = ev.target;
-    const input = createInputField(btn.dataset.type, '');
-    setAdditionalFields([...additionalFields, input]);
+    // const input = createInputField(btn.dataset.type, '', props.data.contactInfo.length);
+    // setAdditionalFields([...additionalFields, input]);
+    const newFields = [...props.data.contactInfo, {[btn.dataset.type]: ''}];
+    props.setData({...props.data, contactInfo: newFields});
   }
 
   function handleBasicInfo(event) {
@@ -68,13 +68,50 @@ export default function Form(props) {
     props.setData({...props.data, [event.target.name]: val});
   }
 
+  function sectionHandler(event, sectionIndex) {
+    const index = parseInt(event.target.dataset.index);
+    const name = event.target.name;
+    let value = event.target.value;
+
+    // "title" is not inside any subsections. Return early
+    if (name === "title") {
+      console.log("editing title")
+      const sections = props.data.sections.map((section, idx) => {
+        if (idx === parseInt(sectionIndex))
+          return {...section, [name]: value};
+        return section;
+      });
+      props.setData({...props.data, "sections": sections})
+      return;
+    }
+
+    // "description" is multiline so needs to be broken into a list
+    // before adding it to json data
+    if (name === "description") value = value.split("\n")
+
+    // First edit the subsection in which update happened
+    const subsections = props.data.sections[sectionIndex].subsections.map((ss, i) => {
+      if (i === index)
+        return {...ss, [name]: value};
+      return ss;
+    });
+
+    // add the subsection to the respective section
+    const sections = props.data.sections.map((s, i) => {
+      if (i === parseInt(sectionIndex)) {
+        return {...s, "subsections": subsections}
+      }
+      return s;
+    });
+
+    props.setData({...props.data, "sections": sections})
+  }
+
   function handleContactInfo(event) {
     const val = event.target.value;
     const name = event.target.name;
     const id = parseInt(event.target.id);
     const newContactInfo = props.data.contactInfo.map((contact, index) => {
-      // const key = Object.keys(contact)[0];
-      // if (key === name) return {[name]: val};
       if (index === id) return {[name]: val};
       return contact;
     });
@@ -97,7 +134,6 @@ export default function Form(props) {
       <fieldset name="contactInfo" classNames="my-2rem" onChange={handleContactInfo}>
         <legend>Contact Info</legend>
         {contactFields}
-        {additionalFields}
         {contactInfoButtons}
       </fieldset>
 
